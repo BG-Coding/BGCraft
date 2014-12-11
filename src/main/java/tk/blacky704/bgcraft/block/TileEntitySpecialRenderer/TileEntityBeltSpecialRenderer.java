f458086479065064c8a5fd72c81f128b17720283
goeiecool9999 <klaas-richel@hotmail.com>
2014-12-11 21:15:43 +0100

    Belt stuff

--- src/main/java/tk/blacky704/bgcraft/block/TileEntitySpecialRenderer/TileEntityBeltSpecialRenderer.java
+++ src/main/java/tk/blacky704/bgcraft/block/TileEntitySpecialRenderer/TileEntityBeltSpecialRenderer.java
@@ -2,6 +2,8 @@ package tk.blacky704.bgcraft.block.TileEntitySpecialRenderer;
 
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import tk.blacky704.bgcraft.block.tileEntity.TileEntityBelt;
+
 import static org.lwjgl.opengl.GL11.*;
 
 /**
@@ -11,14 +13,21 @@ public class TileEntityBeltSpecialRenderer extends TileEntitySpecialRenderer
 {
 
     @Override
-    public void renderTileEntityAt(TileEntity p_147500_1_, double p_147500_2_, double p_147500_4_, double p_147500_6_, float p_147500_8_)
+    public void renderTileEntityAt(TileEntity tileEntity, double p_147500_2_, double p_147500_4_, double p_147500_6_, float p_147500_8_)
     {
+        glPushMatrix();
         glTranslated(p_147500_2_, p_147500_4_, p_147500_6_);
+        glTranslated(0,0.5,0);
+        TileEntityBelt belt = (TileEntityBelt)tileEntity;
+        glTranslated(0,Math.sin(Math.toRadians(belt.testInt))*0.5,0);
+        glDisable(GL_CULL_FACE);
         glBegin(GL_QUADS);
         glVertex3d(0,0,0);
         glVertex3d(1,0,0);
         glVertex3d(1,0,1);
         glVertex3d(0,0,1);
         glEnd();
+        glEnable(GL_CULL_FACE);
+        glPopMatrix();
     }
 }
--- src/main/java/tk/blacky704/bgcraft/block/tileEntity/TileEntityBelt.java
+++ src/main/java/tk/blacky704/bgcraft/block/tileEntity/TileEntityBelt.java
@@ -1,16 +1,28 @@
 package tk.blacky704.bgcraft.block.tileEntity;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.tileentity.TileEntity;
-import tv.twitch.chat.ChatMessage;
 
 /**
  * @author goeiecool9999
  */
 public class TileEntityBelt extends TileEntity
 {
+    public int testInt = 0;
     public TileEntityBelt ()
     {
         System.out.println("tileEntity intialised");
-    };
+    }
+
+    public void updateEntity()
+    {
+        if (testInt <360)
+        {
+            testInt++;
+        } else {
+            testInt++;
+            testInt -= 360;
+        }
+    }
+
+
 }
