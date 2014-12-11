dc6544df18e3e35b6382dfbbcc3346678b8c67ee
goeiecool9999 <klaas-richel@hotmail.com>
2014-12-11 21:18:07 +0100

    Belt stuff

--- src/main/java/tk/blacky704/bgcraft/block/TileEntitySpecialRenderer/TileEntityBeltSpecialRenderer.java
+++ src/main/java/tk/blacky704/bgcraft/block/TileEntitySpecialRenderer/TileEntityBeltSpecialRenderer.java
@@ -20,14 +20,17 @@ public class TileEntityBeltSpecialRenderer extends TileEntitySpecialRenderer
         glTranslated(0,0.5,0);
         TileEntityBelt belt = (TileEntityBelt)tileEntity;
         glTranslated(0,Math.sin(Math.toRadians(belt.testInt))*0.5,0);
+        glDisable(GL_TEXTURE_2D);
         glDisable(GL_CULL_FACE);
         glBegin(GL_QUADS);
+        glColor3d(255,255,255);
         glVertex3d(0,0,0);
         glVertex3d(1,0,0);
         glVertex3d(1,0,1);
         glVertex3d(0,0,1);
         glEnd();
         glEnable(GL_CULL_FACE);
+        glEnable(GL_TEXTURE_2D);
         glPopMatrix();
     }
 }
